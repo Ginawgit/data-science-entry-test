75e014ba1bb69b665f814a76da532ca6f82c154a
Ginawgit <87416497+Ginawgit@users.noreply.github.com>
2026-06-19 22:18:23 +0800

    Update q1.py

--- q1.py
+++ q1.py
@@ -1,15 +1,15 @@
+#Task 1#
 def swap(x, y):
     """
     Task 1
 def swap(x, y):
-    # Check if x and y are numeric (int or float)
-    if not (isinstance(x, (int, float)) and isinstance(y, (int, float))):
+       if not (isinstance(x, (int, float)) and isinstance(y, (int, float))):
         return -1;
     else:
         print(f"Swapped values: x = {y}, y = {x}")
 
 
-# Task 2
+#Task 2#
 swap("apple",10)
 -1
 swap(-9,17)
