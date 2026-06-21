8bd9227d9348ec506179d575b7062b9ad6acdffa
Ginawgit <87416497+Ginawgit@users.noreply.github.com>
2026-06-21 21:21:18 +0800

    Create q8.py

---
+++ q8.py
@@ -0,0 +1,34 @@
+# Task 1
+# Read both prompts above carefully, then answer the questions below as comments.
+
+# Q8a: Which prompt do you think will get a better response from an AI?
+# Your answer: Prompt b
+
+# Q8b: Give TWO reasons to support your choice.
+# Your answer (Reason 1): It is concise in bullet point and it has step sequence.
+# Your answer (Reason 2): It is specific to mention no technical jardon.
+
+# Q8c: What is ONE strength of the prompt you did NOT choose?
+# Your answer: Prompt a, provided the data source and the source format.
+
+
+# Task 2
+# Rewrite either prompt by borrowing ONE element from the other
+# to make it stronger. 
+# Your answer:
+Role: You are a data analyst helping a retail marketing team.
+Task: Analyse customer survey data from a 3-month campaign.
+Data: 500 responses containing age group, product purchased, 
+satisfaction rating (1-5), and written comments.
+Data source: Online survey and in a Excel spreadsheet.
+Steps:
+1. Identify top 3 age groups and products with the lowest satisfaction scores.
+2. Extract the top 3 most common themes from the written comments.
+3. Summarise findings in an executive summary paragraph into a powerpoint slide
+Audience: CEO presentation on Friday.
+Presentation tone: Formal and professional
+Constraints: Keep the summary concise within 250 words and free of technical jargon.
+
+Explain what you borrowed and why.
+Added data source, specify the top 3 age groups and products, specify top 3 most common themes, added presenation tone and limit to the number of words.
+Provide more specific details.
