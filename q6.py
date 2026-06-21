876fc804cad3ec388472d14ee5eb45efccf903e6
Ginawgit <87416497+Ginawgit@users.noreply.github.com>
2026-06-21 16:21:03 +0800

    Create q6.py

---
+++ q6.py
@@ -0,0 +1,15 @@
+#Task 1#
+def find_first_negative(lst):
+  i = 0
+    while i < len(lst):
+        if lst[i] < 0:
+            return lst[i]
+        i += 1
+    return "No negatives"
+
+#Task 2#
+find_first_negative([3, 5, -1, 7, -2, 8])
+-1
+
+find_first_negative([2, 10, 7, 0])
+'No negatives'
